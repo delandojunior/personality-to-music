5471d44270fac34daa312a19ecb6b86dbb8a6857
Delando Junior <delandojuninho@gmail.com>
2017-12-26 13:11:46 -0300

    correção de callback spotify

--- utils/spotify.js
+++ utils/spotify.js
@@ -5,7 +5,7 @@ var db = require('./db.js')
 var spotifyApi = new SpotifyWebApi({
 	clientID        : configAuth.spotifyAuth.clientID,
 	clientSecret    : configAuth.spotifyAuth.clientSecret,
-  	redirectUri : 'http://localhost:3000/login/auth/spotify/callback'
+  	redirectUri : 'https://personality-to-music.herokuapp.com//login/auth/spotify/callback'
 });
 
 
